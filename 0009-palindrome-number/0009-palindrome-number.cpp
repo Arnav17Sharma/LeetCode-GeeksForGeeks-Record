79091b364890ef31620738039f97f5285c3802b7
Arnav Sharma <58596929+Arnav17Sharma@users.noreply.github.com>
2023-10-04 02:15:27 +0530

    Time: 11 ms (44.91%), Space: 6.3 MB (16.09%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(long long x) {
+        long long ans = 0, x1= x;
+        if(x<0){return false;}
+        while(x!=0){
+            long long d = x%10;
+            x /= 10;ans += d;ans *= 10;
+        }
+        if(ans/10 == x1){return true;}
+        return false;
+    }
+};
