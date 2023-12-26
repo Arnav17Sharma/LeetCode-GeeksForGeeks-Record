0ec42c60e5bcb5c780cbc9d8ee2f1d91391a1d1c
Arnav Sharma <58596929+Arnav17Sharma@users.noreply.github.com>
2023-12-27 01:59:55 +0530

    Time: 200 ms (53.11%), Space: 100.7 MB (29.06%) - LeetHub

---
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
+        int m = INT_MIN;
+        for(vector<int> v: matrix){
+            int l = v.size();
+            if(l > m){m = l;}
+        }
+        int N = matrix.size();
+        if(m < N){m = N;}
+        vector<vector<int>> d(2*m-1);
+        if(N==1){return matrix[0];}
+        for(int i=0; i<N; i++){
+            // cout << matrix[i].size();
+            for(int j=0; j<matrix[i].size(); j++){
+                int s = i+j;
+                // cout << matrix[i][j];
+                d[s].push_back(matrix[i][j]);
+            }
+        }
+        vector<int> h;
+        for(vector<int> v: d){
+            reverse(v.begin(), v.end());
+            for(int i=0; i<v.size(); i++){
+                h.push_back(v[i]);
+            }
+        }
+        return h;
+    }
+};
