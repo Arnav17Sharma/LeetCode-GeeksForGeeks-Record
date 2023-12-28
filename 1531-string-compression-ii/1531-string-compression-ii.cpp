8ed2d6a93ca62e684e73ddc946b3e8679c786ea0
Arnav Sharma <58596929+Arnav17Sharma@users.noreply.github.com>
2023-12-28 20:02:53 +0530

    Time: 73 ms (77.53%), Space: 6.7 MB (80.90%) - LeetHub

---
+++ 1531-string-compression-ii/1531-string-compression-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    int getLength(int count){
+        return count == 1 ? 1 : (count<10?2: (count<100 ? 3:4));
+    }
+    
+    
+    int getLengthOfOptimalCompression(string s, int k) {
+        
+        int n = s.size();
+        
+        int dp[n+1][k+1];
+        
+        for(int i=n;i>=0;i--){
+            for(int j=0;j<=k;j++){
+                
+                if(i == n) { dp[n][j] = 0;  continue;}
+                dp[i][j] = j>0 ? dp[i+1][j-1]:INT_MAX;
+                
+                int possible_del = j,count=0;
+                for(int end =i;end<n&&possible_del>=0 ;end++){
+                    if(s[end] == s[i]){
+                        count++;
+                        dp[i][j] = min(dp[i][j] , getLength(count)+dp[end+1][possible_del]);
+                    }
+                    else {
+                        possible_del--;
+                    }
+                }
+                
+                
+            }
+        }
+        
+        return dp[0][k];
+    }
+};
