ed1fde166d641426846160be3a7ca8d23e8d8d1d
Arnav Sharma <58596929+Arnav17Sharma@users.noreply.github.com>
2024-01-08 22:12:18 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (32.71%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,16 +11,23 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode*temp=head;
-        ListNode*temp2=head;
-        int l = 0;
-        while(temp!=NULL){
-            l++;
-            temp=temp->next;
+        // ListNode*temp=head;
+        // ListNode*temp2=head;
+        // int l = 0;
+        // while(temp!=NULL){
+        //     l++;
+        //     temp=temp->next;
+        // }
+        // for(int i=0; i<l/2; i++){
+        //     temp2=temp2->next;
+        // }
+        // return temp2;
+        ListNode*temp1 = head;
+        ListNode*temp2 = head;
+        while(temp2!=NULL &&temp2->next!=NULL){
+            temp1=temp1->next;
+            temp2=temp2->next->next;
         }
-        for(int i=0; i<l/2; i++){
-            temp2=temp2->next;
-        }
-        return temp2;
+        return temp1;
     }
 };
