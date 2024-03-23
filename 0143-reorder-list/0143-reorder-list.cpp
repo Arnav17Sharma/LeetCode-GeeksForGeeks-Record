95bac8c4695eff7f20c7b995f4f71b4f092c32cb
Arnav Sharma <58596929+Arnav17Sharma@users.noreply.github.com>
2024-03-23 19:37:16 +0530

    Time: 29 ms (25.65%), Space: 21.1 MB (99.47%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+    
+    ListNode* reverse(ListNode* head) {
+    ListNode* prev = nullptr;
+    ListNode* curr = head;
+    while (curr) {
+      ListNode* next = curr->next;
+      curr->next = prev;
+      prev = curr;
+      curr = next;
+    }
+    return prev;
+  }
+  ListNode* findMid(ListNode* head) {
+    ListNode* prev = nullptr;
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+      prev = slow;
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+    prev->next = nullptr;
+
+    return slow;
+  }
+
+  void merge(ListNode* l1, ListNode* l2) {
+    while (l2) {
+      ListNode* next = l1->next;
+      l1->next = l2;
+      l1 = l2;
+      l2 = next;
+    }
+  }
+  void reorderList(ListNode* head) {
+    if (!head || !head->next)
+      return;
+    ListNode* mid = findMid(head);
+    ListNode* reversed = reverse(mid);
+    merge(head, reversed);
+  }
+};
