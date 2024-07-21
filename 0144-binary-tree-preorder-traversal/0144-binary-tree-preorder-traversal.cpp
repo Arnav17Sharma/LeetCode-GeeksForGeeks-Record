eda2d6c6f4d3d6d48dbb06b7ad68c70f5ab9cafd
Arnav Sharma <arnavsharma1702@gmail.com>
2024-07-21 17:31:46 +0530

    Time: 4 ms (31.66%), Space: 10.1 MB (12.83%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -20,7 +20,17 @@ public:
         solve(root->right, ans);
     }
     vector<int> preorderTraversal(TreeNode* root) {
-        solve(root, ans);
+        stack<TreeNode*> st;
+        if(root == nullptr)
+            return ans;
+        st.push(root);
+        while(!st.empty()) {
+            TreeNode *node = st.top();
+            ans.push_back(node->val);
+            st.pop();
+            if(node->right) st.push(node->right);
+            if(node->left) st.push(node->left);
+        }
         return ans;
     }
 };
