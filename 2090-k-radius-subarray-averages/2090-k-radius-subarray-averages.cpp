261483a06b44d6ce70a719e369cbe3fd926f24b0
Arnav Sharma <asharma020.btech2022@it.nitrr.ac.in>
2024-08-25 12:01:31 +0530

    Time: 139 ms (72.69%), Space: 132.3 MB (89.23%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> getAverages(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(k == 0)
+            return nums;
+        
+        vector<int> result(n, -1);
+        
+        if(2*k+1 > n)
+            return result;
+        
+        long long window = 0;
+        int left = 0;
+        int right = 2*k;
+        int i = k;
+        for(int i=left; i<=right; i++) {
+            window += nums[i];
+        }
+        int count = 2*k + 1;
+        result[i] = window / count;
+        i++;
+        right++;
+
+        while(right < n) {
+            int out = nums[left];
+            int in = nums[right];
+
+            window += (in - out);
+            result[i] = window / count;
+            i++;
+            right++;
+            left++;
+        }
+        return result;
+    }
+};
