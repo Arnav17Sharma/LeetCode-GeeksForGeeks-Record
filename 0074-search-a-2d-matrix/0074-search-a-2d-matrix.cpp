ab3032003ae4a82bbbef4ddde8cafcaca63450e4
Arnav Sharma <asharma020.btech2022@it.nitrr.ac.in>
2024-08-31 23:39:57 +0530

    Time: 2 ms (68.56%), Space: 12.1 MB (78.99%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i=0; i<matrix.size(); i++){
-            for(int j=0; j<matrix[0].size(); j++){
-                if(matrix[i][j] == target)
-                    return true;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int l = 0, r = m*n - 1;
+        while (l <= r) {
+            int mid = l + (r-l)/2;
+            int x = mid / n;
+            int y = mid % n;
+            if(matrix[x][y] > target) {
+                r = mid-1;
+            } else if(matrix[x][y] < target) {
+                l = mid+1;
+            } else {
+                return true;
             }
         }
         return false;
