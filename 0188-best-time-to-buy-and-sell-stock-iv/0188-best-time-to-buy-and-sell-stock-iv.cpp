6aa2bf7c58d7d437efaf371d8abf96d3ac79671a
Arnav Sharma <asharma020.btech2022@it.nitrr.ac.in>
2024-10-02 17:58:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& Arr) {
+        int n = Arr.size();
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(k+1, vector<int>(3, 0)));
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= k; cap++) {
+                    if (buy == 0) {
+                        dp[ind][buy][cap] = max(0 + dp[ind + 1][0][cap], -Arr[ind] + dp[ind + 1][1][cap]);
+                    }
+
+                    if (buy == 1) {
+                        dp[ind][buy][cap] = max(0 + dp[ind + 1][1][cap], Arr[ind] + dp[ind + 1][0][cap - 1]);
+                    }
+                }
+            }
+        }
+        return dp[0][0][k];
+    }
+};
