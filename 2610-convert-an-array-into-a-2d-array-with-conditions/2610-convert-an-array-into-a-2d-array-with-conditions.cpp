66eed27f486c43d3681884e47be8aa960fcbb6ef
Arnav Sharma <asharma020.btech2022@it.nitrr.ac.in>
2025-01-15 00:35:46 +0530

    Time: 0 ms (100%), Space: 34 MB (86.59%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,12 +1,15 @@
 class Solution {
- public:
-  vector<vector<int>> findMatrix(vector<int>& nums) {
-    vector<vector<int>> ans;
-    vector<int> count(nums.size() + 1);
-    for (const int num : nums) {
-      if (++count[num] > ans.size()){ans.push_back({});}
-      ans[count[num] - 1].push_back(num);
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<int> freq(nums.size() + 1);
+        vector<vector<int>> ans;
+        for (int c : nums) {
+            if (freq[c] >= ans.size()) {
+                ans.push_back({});
+            }
+            ans[freq[c]].push_back(c);
+            freq[c]++;
+        }
+        return ans;
     }
-    return ans;
-  }
 };
